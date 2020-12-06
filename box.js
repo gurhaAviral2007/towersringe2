b7d211229e646bc68e1b2648cc22f62a05d0c293
gurhaAviral2007 <71877050+gurhaAviral2007@users.noreply.github.com>
2020-12-06 14:01:17 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,36 @@
+class Block{
+  constructor(x, y, width, height,color) {
+      var options = {
+         
+          restitution :0.4,
+          friction :0.4,
+          //isStatic:true
+         
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.color= color;
+      this.Visiblity = 255;
+      World.add(world, this.body);
+    }
+    display(){
+       if(this.body.speed < 8){
+      var angle = this.body.angle;
+      var pos= this.body.position;
+      
+      push();      
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill (this.color)
+      rect(0,0,this.width, this.height);
+      pop();
+   }
+   else{
+    World.remove(world, this.body);
+    this.Visiblity = this.Visiblity - 5;
+      tint(255,this.Visiblity);
+   }
+    }
+}
